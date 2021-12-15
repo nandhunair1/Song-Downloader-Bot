8519d1f723bcfb05b64bab1b9fbb8995704917b2
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-12-15 09:29:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ class Config(object):
     DATABASE = os.environ.get("DB_URI")
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1961162381").split())
     SUPPORT = os.environ.get("SUPPORT")
+    PICS = os.environ.get("PICS", "https://telegra.ph/file/3cddff05c6461a471af4c.jpg")).split()
+
